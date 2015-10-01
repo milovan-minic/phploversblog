7dfe0c22f3b6a6b2acef1548a141121894067fe0
milovan-minic <milovan.minic@gmail.com>
2015-10-01 20:41:11 +0200

    Napravljen layout aplikacije. Zavrsen 2. video za projekat phploversblog (lbba - 15.mp4)
    Dodat Bootstap, napravljene male izmene i kreiran sadrzaj za index, post i posts strane

---
+++ posts.php
@@ -0,0 +1,27 @@
+<?php include "includes/header.php"; ?>
+
+    <div class="blog-post">
+        <h2 class="blog-post-title">International PHP conference 2015</h2>
+        <p class="blog-post-meta">November 1, 2015 by <a href="#">Mark</a></p>
+
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+
+        <a class="readmore" href="post.php?id=1">Read More</a>
+
+    </div><!-- /.blog-post -->
+
+    <div class="blog-post">
+        <h2 class="blog-post-title">PHP 5.6.0beta4 Released</h2>
+        <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
+
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+
+        <a class="readmore" href="post.php?id=2">Read More</a>
+
+    </div><!-- /.blog-post -->
+
+<?php include "includes/footer.php"; ?>
